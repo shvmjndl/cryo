9ee6dbf406086d52ce1b880f6db8892a9706189f
shivam <shivam.jindal@irame.ai>
2026-04-21 03:26:20 +0530

    better cryo.sh

--- cryo.sh
+++ cryo.sh
@@ -1,1 +1,18 @@
-docker compose down && docker compose up -d && docker logs cryo-api-1 -f
+#!/bin/bash
+
+CMD=$1
+
+case "$CMD" in
+  logs)
+    docker logs cryo-api-1 -f
+    ;;
+  down)
+    docker compose down
+    ;;
+  up|"")
+    docker compose down && docker compose up -d && docker logs cryo-api-1 -f
+    ;;
+  *)
+    echo "Usage: bash run.sh [up|down|logs]"
+    ;;
+esac
